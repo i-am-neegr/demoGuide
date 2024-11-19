7972289eb6eabf15ff3ae3de311840ec339a7b5b
i-am-neegr <martial.artem@gmail.com>
2024-11-19 14:24:05 +0300

    Initial commit

---
+++ src/main/java/com/example/demoGuide/DemoGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGuideApplication.class, args);
+	}
+
+}
