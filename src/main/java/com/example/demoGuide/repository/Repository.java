a0bb07f32b3bed3d89684d428425d04bec0b4c79
i-am-neegr <166641557+i-am-neegr@users.noreply.github.com>
2024-11-26 14:23:01 +0300

    swaaaaaag

--- src/main/java/com/example/demoGuide/repository/Repository.java
+++ src/main/java/com/example/demoGuide/repository/Repository.java
@@ -10,6 +10,35 @@ import org.apache.tomcat.util.json.JSONParser;
 import org.json.JSONObject;
 
 public class Repository {
-    JSONParser parser = new JSONParser();
-    private JSONObject distanceMatrix = parser.parse(new FileReader("data/di"))
+
+    public static JSONObject getDistanceJSON() {
+        StringBuilder jsonBuilder = new StringBuilder();
+
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("data\\dist_graph.json"))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                jsonBuilder.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new JSONObject(jsonBuilder.toString());
+    }
+
+    public static JSONObject allInfoJSON () {
+        StringBuilder jsonBuilder = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("data\\all_info.json"))){
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                jsonBuilder.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new JSONObject(jsonBuilder.toString());
+    }
+
+    public static JSONObject
 }
